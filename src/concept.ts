7e27eac627332f377c13ee9f022f2408c560edca
zoenetic <56755170+zoenetic@users.noreply.github.com>
2026-08-13 20:04:49 +0100

    prepublish

--- src/concept.ts
+++ src/concept.ts
@@ -2,13 +2,19 @@ import type { Fill } from "./fill";
 import { type Gap, gap } from "./gap";
 
 export const kind: unique symbol = Symbol("kind");
+export const shape: unique symbol = Symbol("shape");
 
-export type Kind = "def" | "ref" | "is";
+export type Kind = "def" | "ref" | "of" | "many" | "maybe" | "oneOf";
 
-export interface Concept<G extends string = Gap, F extends Fill = {}> {
+export interface Concept<
+	G extends string = Gap,
+	F extends Fill = {},
+	T = unknown,
+> {
 	readonly [kind]: Kind;
 	readonly prompt?: string;
 	readonly fill?: F;
 	readonly to?: Concept<any, any>;
 	readonly [gap]?: G;
+	readonly [shape]?: T;
 }
